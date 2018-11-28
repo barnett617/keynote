7617d04e6a1686e92025ea3d9a5e33d8d5e270cc
wangxin <binaryaoe@163.com>
2018-11-28 17:50:12 +0800

    initial

---
+++ src/app.js
@@ -0,0 +1,58 @@
+import Taro, { Component } from '@tarojs/taro'
+import '@tarojs/async-await'
+import { Provider } from '@tarojs/redux'
+
+import Index from './pages/index'
+
+import configStore from './store'
+
+import './app.scss'
+
+const store = configStore()
+
+class App extends Component {
+
+  config = {
+    pages: [
+      'pages/index/index'
+    ],
+    window: {
+      backgroundTextStyle: 'light',
+      navigationBarBackgroundColor: '#fff',
+      navigationBarTitleText: 'WeChat',
+      navigationBarTextStyle: 'black'
+    },
+    plugins: {
+      sdkPlugin: {
+        "version": "1.10.0",
+        "provider": "wxc6b86e382a1e3294"
+      }
+    }
+  }
+
+  componentDidMount () {
+    const clientId = 'dd35dbb2deae1a25bc6d'
+    Taro.BaaS = Taro.requirePlugin('sdkPlugin')
+    //让插件帮助完成登录、支付等功能
+    Taro.BaaS.wxExtend(Taro.login,
+      Taro.getUserInfo,
+      Taro.requestPayment)
+    Taro.BaaS.init(clientId)
+  }
+
+  componentDidShow () {}
+
+  componentDidHide () {}
+
+  componentCatchError () {}
+
+  render () {
+    return (
+      <Provider store={store}>
+        <Index />
+      </Provider>
+    )
+  }
+}
+
+Taro.render(<App />, document.getElementById('app'))
---
+++ src/pages/index/index.js
@@ -0,0 +1,52 @@
+import Taro, { Component } from '@tarojs/taro'
+import { View, Button, Text } from '@tarojs/components'
+import { connect } from '@tarojs/redux'
+
+import { add, minus, asyncAdd } from '../../actions/counter'
+
+import './index.scss'
+
+
+@connect(({ counter }) => ({
+  counter
+}), (dispatch) => ({
+  add () {
+    dispatch(add())
+  },
+  dec () {
+    dispatch(minus())
+  },
+  asyncAdd () {
+    dispatch(asyncAdd())
+  }
+}))
+class Index extends Component {
+
+    config = {
+    navigationBarTitleText: '蛤一下'
+  }
+
+  componentWillReceiveProps (nextProps) {
+    console.log(this.props, nextProps)
+  }
+
+  componentWillUnmount () { }
+
+  componentDidShow () { }
+
+  componentDidHide () { }
+
+  handleClick = () => {
+    
+  }
+
+  render () {
+    return (
+      <View className='index'>
+        <Button className='main-btn' onClick={this.handleClick}>蛤一下！</Button>
+      </View>
+    )
+  }
+}
+
+export default Index
